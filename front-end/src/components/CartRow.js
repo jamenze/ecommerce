1a1b6a9e9d648a8555993ceba41e94d90d7471df
Jennifer Menze <jamenze@gmail.com>
2017-12-18 15:34:52 -0500

    added cartrow

---
+++ front-end/src/components/CartRow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CartRow(props) {
+	const product = props.product;
+	return (
+		<h1>{product}
+	)
+}
+
+export default CartRow;
--- front-end/src/containers/Cart.js
+++ front-end/src/containers/Cart.js
@@ -5,6 +5,11 @@ import GetCart from '../actions/GetCart';
 
 class Cart extends Component{
 
+	constructor() {
+		super();
+		this.makePayment = this.makePayment.bind(this);
+	}
+
 	componentDidMount(){
 		console.log(this.props.auth);
 		if(this.props.auth.token === undefined){
@@ -16,8 +21,45 @@ class Cart extends Component{
 		}
 	}
 
+	makePayment() {
+        var handler = window.StripeCheckout.configure({
+            key: 'pk_test_aj8GszmxnsDvaNM10ZFt9ECA',
+            locale: 'auto',
+            token: (token) => {
+                var theData = {
+                    amount: 10 * 100,
+                    stripeToken: token.id,
+                    userToken: this.props.tokenData,
+                }
+                axios({
+                    method: 'POST',
+                    url: window.hostAdress'/stripe',
+                    data: theData
+                }).done((data) => {
+                    console.log(data);
+                    if (data.msg === 'paymentSuccess') {
+
+                    }
+                });
+            }
+        });
+        handler.open({
+            name: "Pay Now",
+            description: 'Pay Now',
+            amount: 10 * 100
+        })
+    }
+
 	render(){
 		console.log(this.props.cart);
+		if(!this.props.cart.totalItems){
+			return(
+				// if this return occurs, the render is DONE
+				<div>
+					<h3>Your cart is empty! Get shopping!</h3>
+				</div>
+			)
+		}
 		return(
 			<h1>Cart Page</h1>
 		)
